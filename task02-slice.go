8920ca929fd32451464d22a886d75cda7c89844b
Sharmiko <sharmiko123@gmail.com>
2022-05-09 22:31:19 +0400

    update task 2

--- task02-slice.go
+++ task02-slice.go
@@ -3,8 +3,18 @@ package homework
 import "sort"
 
 func reverse(input []int64) (result []int64) {
-	sort.Slice(input, func(x, y int) bool {
-		return input[x] > input[y]
+	isSorted := sort.SliceIsSorted(input, func(x, y int) bool {
+		return input[x] < input[y]
 	})
+	if isSorted {
+		sort.Slice(input, func(x, y int) bool {
+			return input[x] > input[y]
+		})
+	} else {
+		sort.Slice(input, func(x, y int) bool {
+			return input[x] < input[y]
+		})
+	}
+
 	return input
 }
